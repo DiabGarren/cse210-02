9a9f8258a13c43b57285c52d426f169200182b1c
AlisonMazzarella <maz12005@byui.edu>
2022-10-05 09:10:33 +0200

    Changed some of the defined functions so their purpose is more clear. We have 3 functions now, main, hilo, and calculations. Added while loops and returns. Marked defects with comments.

--- main.py
+++ main.py
@@ -1,30 +1,74 @@
 import random as r 
 
+
 def main(): 
-    """ To play the game is very simple, you will start with 300 points. To begin you will be shown a card at random. You will need
+    return(guess)
+
+print("""To play the game is very simple, you will start with 300 points. To begin you will be shown a card at random. You will need
 to guess whether you think the next card will be higher in value or lower. Guess right and you earn 100 points. Guess wrong and you 
 will lose 75. Reach 1,000 points and you win the game, reach 0 and you lose. 
-Good luck! 
-"""
-range = r.randint(1,13) #set between 1 and 13 because cards only have value between 1 and 13
-running_total = 0
+Good luck!""")
 
-while running_total < 1000:
-    print("Will the next card be higher or lower? ")
+card_value = r.randint(1,13)
 
-    if running_total == 1000:
-        print("Congrats, you won the game! ")
+guess = str(input("Please enter whether you think the next card will be higher or lower. (h/l)\n").lower())
+#need to add where the program initiates the game by saying the card value is x
+
+
+
+def hilo_game():
+    return r.randint(card_value)
+
+keep_playing = True
+has_points = True
+
+while keep_playing: 
+    keep_playing = str(input("Would you like to keep playing? (y/n)\n").lower())
+
+    if has_points < 0: 
+        keep_playing()
+
+    elif has_points > 1000: 
+        str(input("You have won the game. Would you like to play again? (y/n)\n").lower())
 
     else: 
-        answer = input("Would you like to play again? Yes or No. ")
-        if answer == 'yes': 
-            if answer == 'no':
-                quit
+        str(input("You have lost the game. Would you like to play again? (y/n)\n").lower())
+#program does not quit when you click no 
 
-    def store_calculations(): 
-        turn = int(answer)
-        calculations = calculations + turn 
-        print("You have {} points currently. ")
+
+
+def calculation():
+    return(player_score)
+
+player_score = []
+x = 300
+
+#trying to create a list where the original score of 300 is kept, and after each round the score is appended, and displays the current score til they either each 1,000 and win or 0 and lose
+while guessing:
+    if guess > card_value:
+        for i in range (1, 1000): 
+            x -= 75
+            player_score.append(x)
+            print("Too high, you lose 75 points. ")
+            print("Current score = {player_score}." )
+
+    elif guess < card_value:
+        for i in range (1, 1000): 
+            x -= 75
+            player_score.append(x)
+            print("Too low, you lose 75 points. ")
+            print("Current score = {player_score}." )
+
+    else:
+        for i in range (1, 1000):
+            x += 100
+            player_score.append(x)
+            print("Nice, you got it! You earned 100 points. ")
+            print("Current score = {player_score}." )
+            guessing = False
+
+
+
 
 
 if __name__ == "main":
